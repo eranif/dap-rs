9762fd952f509f200c62efb50b66379695fe13b7
Eran Ifrah <eran@codelite.org>
2023-01-29 20:05:33 +0200

    simplified the server code

--- src/prelude.rs
+++ src/prelude.rs
@@ -1,13 +1,12 @@
 #[doc(hidden)]
 pub use crate::{
-  adapter::Adapter,
-  client::StdoutWriter,
-  errors::ClientError,
-  events::{self, Event},
-  line_reader::{FileLineReader, LineReader},
-  requests::{self, Command, Request},
-  responses::{self, Response, ResponseBody},
-  reverse_requests::{ReverseCommand, ReverseRequest},
-  server::Server,
-  types,
+    adapter::Adapter,
+    client::StdoutWriter,
+    events::{self, Event},
+    line_reader::{FileLineReader, LineReader},
+    requests::{self, Command, Request},
+    responses::{self, Response, ResponseBody},
+    reverse_requests::{ReverseCommand, ReverseRequest},
+    server::Server,
+    types,
 };
